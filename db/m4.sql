20895a209300e34af606cfa311da013f5093048a
Dave Scott <davejs133@gmail.com>
2016-12-16 14:08:19 +0000

    add user, account & transaction model files and initialize all

--- db/m4.sql
+++ db/m4.sql
@@ -1,9 +1,9 @@
-DROP TABLE users IF EXISTS;
-DROP TABLE accounts IF EXISTS;
-DROP TABLE transactions IF EXISTS;
+IF EXISTS DROP TABLE users;
+IF EXISTS DROP TABLE accounts;
+IF EXISTS DROP TABLE transactions;
 
 CREATE TABLE users(
-id SERIAL, PRIMARY KEY,
+id SERIAL PRIMARY KEY,
 name VARCHAR(255),
 total_income MONEY,
 total_expeses MONEY
@@ -11,7 +11,7 @@ total_expeses MONEY
 
 
 CREATE TABLE accounts(
-id SERIAL4, PRIMARY KEY,
+id SERIAL4 PRIMARY KEY,
 type VARCHAR(255),
 transactions SERIAL4 references transactions(id),
 income MONEY,
@@ -20,7 +20,7 @@ customer SERIAL4 references users(id)
 );
 
 CREATE TABLE transactions(
-id SERIAL4, PRIMARY KEY,
+id SERIAL4 PRIMARY KEY,
 type VARCHAR(255),
 merchant VARCHAR(255),
 tag VARCHAR(255),
