ea0136f535b3787121a9fe372709a68dd84bef81
Dave Scott <davejs133@gmail.com>
2016-12-16 13:29:37 +0000

    add database, sql file & sql runner

---
+++ db/m4.sql
@@ -0,0 +1,29 @@
+DROP TABLE users IF EXISTS;
+DROP TABLE accounts IF EXISTS;
+DROP TABLE transactions IF EXISTS;
+
+CREATE TABLE users(
+id SERIAL, PRIMARY KEY,
+name VARCHAR(255),
+total_income MONEY,
+total_expeses MONEY
+);
+
+
+CREATE TABLE accounts(
+id SERIAL4, PRIMARY KEY,
+type VARCHAR(255),
+transactions SERIAL4 references transactions(id),
+income MONEY,
+expeses MONEY,
+customer SERIAL4 references users(id)
+);
+
+CREATE TABLE transactions(
+id SERIAL4, PRIMARY KEY,
+type VARCHAR(255),
+income MONEY,
+expeses MONEY
+);
+
+
